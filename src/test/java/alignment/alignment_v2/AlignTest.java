567b65b17fabf23c851da86882a235074954acac
euf <euf@ornl.gov>
2014-04-24 18:18:00 -0400

    shorter test

--- src/test/java/alignment/alignment_v2/AlignTest.java
+++ src/test/java/alignment/alignment_v2/AlignTest.java
@@ -438,7 +438,7 @@ extends TestCase
 
 
 	/**
-	 * Tests loading & querying from realistic graphson file (~2M file)
+	 * Tests loading & querying from realistic graphson file (~220K file)
 	 * @throws IOException 
 	 */
 	public void testGraphsonFile() throws IOException
@@ -447,7 +447,7 @@ extends TestCase
 		a.removeAllVertices();
 		a.removeAllEdges();
 
-		String test_graphson_verts = org.apache.commons.io.FileUtils.readFileToString(new File("resources/metasploit.json"), "UTF8");
+		String test_graphson_verts = org.apache.commons.io.FileUtils.readFileToString(new File("resources/metasploit_short.json"), "UTF8");
 		//System.out.println(test_graphson_verts);
 		a.load(test_graphson_verts);
 
