431ac82b8b058892bd41fc05ddabcf130fb408f8
euf <euf@ornl.gov>
2015-01-12 15:01:48 -0500

    modified Align to use any Rexster connection

--- src/main/java/alignment/alignment_v2/Align.java
+++ src/main/java/alignment/alignment_v2/Align.java
@@ -38,38 +38,22 @@ public class Align
 	private Logger logger = null;
 	private Map<String, String> vertIDCache = null;
 	private ConfigFileLoader config = null;
-	
-	//TODO: these timeouts seem to do nothing.  If the server is down, it seems to wait (& apparently retry) forever.
-	// should probably submit a bug report for this.
-    private static final BaseConfiguration configOpts = new BaseConfiguration() {{
-        addProperty(RexsterClientTokens.CONFIG_HOSTNAME, "localhost");
-        addProperty(RexsterClientTokens.CONFIG_PORT, 8184);
-        addProperty(RexsterClientTokens.CONFIG_TIMEOUT_CONNECTION_MS, 18000);
-        addProperty(RexsterClientTokens.CONFIG_TIMEOUT_WRITE_MS, 4000);
-        addProperty(RexsterClientTokens.CONFIG_TIMEOUT_READ_MS, 16000);
-        addProperty(RexsterClientTokens.CONFIG_MAX_ASYNC_WRITE_QUEUE_BYTES, 512000);
-        addProperty(RexsterClientTokens.CONFIG_MESSAGE_RETRY_COUNT, 4);
-        addProperty(RexsterClientTokens.CONFIG_MESSAGE_RETRY_WAIT_MS, 50);
-        addProperty(RexsterClientTokens.CONFIG_LANGUAGE, "groovy");
-        addProperty(RexsterClientTokens.CONFIG_GRAPH_OBJECT_NAME, "g");
-        addProperty(RexsterClientTokens.CONFIG_GRAPH_NAME, "graph"); //not rename-able?  Seems like a Titan thing?
-        addProperty(RexsterClientTokens.CONFIG_TRANSACTION, true);
-        addProperty(RexsterClientTokens.CONFIG_SERIALIZER, MsgPackSerializer.SERIALIZER_ID);
-    }};
     
-    public Align() {
-    	
-    	//loading configuration file into HashMap
+    public Align(){
+    	this(null);
+    }
+    
+    public Align(RexsterClient c) {
+    	client = c;
 
     	logger = LoggerFactory.getLogger(Align.class);
     	vertIDCache = new HashMap<String, String>(10000);
+    	//loading configuration file into HashMap
     	config = new ConfigFileLoader();
     	
     	try {
     		List<Map<String,Object>> result;
-			logger.info("connecting to DB...");
-			client = RexsterClientFactory.open(configOpts);		//created at the beginning of the file
-	//		System.out.println("client" + client);
+    		if (client == null) client = getDefaultClient();
 			//configure vert indices needed
 			//List currentIndices = client.execute("g.getManagementSystem().getGraphIndexes(Vertex.class)");
 			List currentIndices = client.execute("g.getIndexedKeys(Vertex.class)");
@@ -127,6 +111,33 @@ public class Align
 		closeClient();
 		super.finalize();
 	}
+	
+    private static RexsterClient getDefaultClient() throws Exception{
+    	RexsterClient client = null;
+    	
+    	//TODO: these timeouts seem to do nothing.  If the server is down, it seems to wait (& apparently retry) forever.
+    	// should probably submit a bug report for this.
+        BaseConfiguration configOpts = new BaseConfiguration() {{
+            addProperty(RexsterClientTokens.CONFIG_HOSTNAME, "localhost");
+            addProperty(RexsterClientTokens.CONFIG_PORT, 8184);
+            addProperty(RexsterClientTokens.CONFIG_TIMEOUT_CONNECTION_MS, 18000);
+            addProperty(RexsterClientTokens.CONFIG_TIMEOUT_WRITE_MS, 4000);
+            addProperty(RexsterClientTokens.CONFIG_TIMEOUT_READ_MS, 16000);
+            addProperty(RexsterClientTokens.CONFIG_MAX_ASYNC_WRITE_QUEUE_BYTES, 512000);
+            addProperty(RexsterClientTokens.CONFIG_MESSAGE_RETRY_COUNT, 4);
+            addProperty(RexsterClientTokens.CONFIG_MESSAGE_RETRY_WAIT_MS, 50);
+            addProperty(RexsterClientTokens.CONFIG_LANGUAGE, "groovy");
+            addProperty(RexsterClientTokens.CONFIG_GRAPH_OBJECT_NAME, "g");
+            addProperty(RexsterClientTokens.CONFIG_GRAPH_NAME, "graph"); //not rename-able?  Seems like a Titan thing?
+            addProperty(RexsterClientTokens.CONFIG_TRANSACTION, true);
+            addProperty(RexsterClientTokens.CONFIG_SERIALIZER, MsgPackSerializer.SERIALIZER_ID);
+        }};
+        Logger logger = LoggerFactory.getLogger(Align.class);
+        logger.info("connecting to DB...");
+		client = RexsterClientFactory.open(configOpts);		//this just throws "Exception."  bummer.
+//		System.out.println("client" + client);
+    	return client;
+    }
     
 	//only needed for tests
 	public RexsterClient getClient(){
