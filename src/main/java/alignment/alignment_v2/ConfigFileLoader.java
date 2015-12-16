b90d6c67feaf0fdc140271610e1dd74e9314ec4f
Maria Vincent <mvincen2@vols.utk.edu>
2015-12-15 21:28:01 -0500

    loading config files for alignment

--- src/main/java/alignment/alignment_v2/ConfigFileLoader.java
+++ src/main/java/alignment/alignment_v2/ConfigFileLoader.java
@@ -1,128 +1,44 @@
 package alignment.alignment_v2;
 
-import java.io.*;
-//import java.nio.file.Files;
-import java.util.*;
+import java.io.IOException;
 
-import org.apache.commons.configuration.Configuration;
-//import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.json.JSONArray;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.yaml.snakeyaml.Yaml;
+import org.json.JSONArray;
 
 public class ConfigFileLoader {
-
-	//private static final String CONFIG_FILE = "alignment.yaml";
-	private static final String CONFIG_FILE = "stucco_schema.json";
-	String path = null;
-
-	public ConfigFileLoader(){
-		this.path = CONFIG_FILE;
-	}
-
-	public ConfigFileLoader(String path){
-		this.path = path;
-	}
-
-	public Map<String, Map<String, Object>> getVertexConfig(String configHeading){
-		//return getVertexConfigFromYaml(configHeading);
-		return getVertexConfigFromJsonSchema(configHeading);
-	}
+	private String STIX_ONTOLOGY = "resources/ontology/stix_ontology.json";
+	private String VERTEX_TYPE_CONFIG = "resources/ontology/vertex_type_config.json";
 	
-	private Map<String, Map<String, Object>> getVertexConfigFromYaml(String configHeading) {
+	private JSONObject ontology = null;
+	private JSONObject vertexTypeConfig = null;
 
-		ArrayList<Object> array = null;
-
-		Yaml yamlReader = new Yaml();
-		InputStream stream = ConfigFileLoader.class.getClassLoader().getResourceAsStream(path);
-		Map<String, Map<String, Object>> configMap = (Map<String, Map<String, Object>>) yamlReader.load(stream);
-		Map<String, Map<String, Object>> vertexConfigMap = new HashMap<String, Map<String, Object>>();
-
-		Map<String, Object> map = (Map<String, Object>) configMap.get("vertices");
-
-		Map<String, Object> vertMap = (Map<String, Object>) map.get(configHeading);
-		Map<String, Object> propMap = null;
-		for (String s1 : vertMap.keySet())	{
-			propMap = (HashMap<String, Object>)vertMap.get(s1);
-			vertexConfigMap.put(s1, propMap);
-		}
-
-		return vertexConfigMap;
-	}
-	
-	//returns a map of prop names to merge methods, for each vert name
-    private Map<String, Map<String, Object>> getVertexConfigFromJsonSchema(String vertexName){
-    	InputStream stream = ConfigFileLoader.class.getClassLoader().getResourceAsStream(path);
-    	JSONTokener j = new JSONTokener(stream);
-    	JSONObject ontology = new JSONObject(j); 
-
-    	HashMap<String, Map<String, Object>> vertexConfigMap = new HashMap<String, Map<String, Object>>();
-    	JSONObject definitions = ontology.getJSONObject("definitions");
-    	
-    	HashMap<String, Object> propConfig = null;
-    	JSONObject currVert = definitions.getJSONObject(vertexName);
-    	JSONArray items = currVert.getJSONArray("allOf");
-    	
-    	for(int i=0; i<items.length(); i++){
-    		JSONObject item = items.getJSONObject(i);
-    		if(item.has("properties")){
-    			JSONObject currProps = item.getJSONObject("properties");
-        		Iterator<String> k = currProps.keys();
-        		while(k.hasNext()){
-        			String prop = k.next();
-        			propConfig = new HashMap<String, Object>();
-        			
-        			String comparisonFunction = currProps.getJSONObject(prop).optString("comparisonFunction");
-        			if(comparisonFunction == null || comparisonFunction == "")
-        				comparisonFunction = "none";
-        			propConfig.put("comparisonFunction", comparisonFunction);
-        			
-        			Double comparisonWeight = currProps.getJSONObject(prop).optDouble("comparisonWeight");
-        			if(comparisonWeight == null || comparisonWeight == Double.NaN)
-        				comparisonWeight = 0.0;
-        			propConfig.put("comparisonWeight", comparisonWeight);
-        			
-        			String resolutionFunction = currProps.getJSONObject(prop).optString("resolutionFunction");
-        			if(resolutionFunction == null || resolutionFunction == "")
-        				resolutionFunction = "keepNew";
-        			propConfig.put("resolutionFunction", resolutionFunction);
-        			
-        			vertexConfigMap.put(prop, propConfig);
-        		}
-    		}
-    	}
-    	return vertexConfigMap;
-	}
-	
-	public static Configuration dbConfigFromFile(String configFilePath){
-		File configFile = new File(configFilePath);
-		Map<String, Object> fullConfigMap = configMapFromFile(configFile);
-		Map<String, Object> dbConfigMap = (Map<String, Object>) fullConfigMap.get("database_connection");
-		PropertiesConfiguration dbConfig = new PropertiesConfiguration();
-		for(String key : dbConfigMap.keySet()){
-			dbConfig.addProperty( key, dbConfigMap.get(key));
-		}
-		return dbConfig;
-	}
-	
-	public static Map<String, Object> configMapFromFile(File configFile){
-		Map<String, Object> configMap = null;
+	public ConfigFileLoader() {
 		try {
-			Yaml yaml = new Yaml();
-			InputStream stream = new FileInputStream(configFile);
-			
-			configMap = (Map<String, Object>) yaml.load( stream );
-			
-			//config.load(configFile);
-		
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			/* required to map new incomming stix xml to vertesType (like is it IP, Port, etc ?) */
+			vertexTypeConfig = new JSONObject(new String(Files.readAllBytes(Paths.get(VERTEX_TYPE_CONFIG))));
+			/* required to construct a graph, to do all the comparisons, and stix xml editing */
+			ontology = new JSONObject(new String(Files.readAllBytes(Paths.get(STIX_ONTOLOGY))));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return configMap;
+	}
+
+	public JSONObject getStuccoOntology() {
+		return ontology;
+	}
+	
+	public JSONObject getVertexTypeConfig() {
+		return vertexTypeConfig;
+	}
+
+	public JSONObject getVertexOntology(String vertexType) {
+		if (ontology.getJSONObject("definitions").has(vertexType)) {
+			return ontology.getJSONObject("definitions").getJSONObject(vertexType);
+		} 
+		
+		return null;
 	}
 }
-
-
