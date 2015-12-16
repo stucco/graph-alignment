99173adc5860a60418a4d57a8509deb73e03fe46
Maria Vincent <mvincen2@vols.utk.edu>
2015-12-15 21:27:28 -0500

    comparison class to compare json and xml

--- src/main/java/alignment/alignment_v2/Compare.java
+++ src/main/java/alignment/alignment_v2/Compare.java
@@ -2,117 +2,84 @@ package alignment.alignment_v2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
+import org.json.JSONObject;
 import org.json.JSONArray;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import alignment.alignment_v2.comparisons.*;
+import alignment.alignment_v2.comparisons.WHIRL;
+import alignment.alignment_v2.comparisons.CosineSimilarity;
+import alignment.alignment_v2.comparisons.SmithWaterman;
 
-public class Compare {
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.Attribute;
 
-	/*
-	private Align align;
-	private Logger logger;
-	private Comparison comparison;
-	private WHIRL whirl;
-	 */
+import org.apache.commons.lang.StringUtils;
 
-	public static double compareVertices(Map<String, Object> vert1, Map<String, Object> vert2, Map<String, Map<String, Object>> configProperties){
-		Map<String, Object> props1 = (Map<String, Object>)vert1.get("_properties");
-		Map<String, Object> props2 = (Map<String, Object>)vert2.get("_properties");
-		List<String> props = new ArrayList<String>(); 
-		Object props1keys[] = props1.keySet().toArray();
-		for(int i=0; i<props1keys.length; i++){
-			if(props2.containsKey((String)props1keys[i])){
-				props.add((String)props1keys[i]);
+public class Compare extends PreprocessSTIXwithJDOM2 {
+
+	private CosineSimilarity cs = null;
+	private WHIRL whirl = null;
+
+	public double compareVertices(JSONObject vertexOne, JSONObject vertexTwo, JSONObject vertexOntology) {
+		double score = 0.0;
+		JSONObject properties = vertexOntology.getJSONObject("properties");
+		
+		for (Object propertyObject : properties.keySet()) {
+			String property = propertyObject.toString();
+			if (vertexOne.has(property) && vertexTwo.has(property)) {
+				double newScore = compareProperties(vertexOne.get(property), vertexTwo.get(property), properties.getJSONObject(property));
+				score = score + newScore;
 			}
 		}
-		Double score = 0.0;
-		for(int i=0; i<props.size(); i++){
-			String p = props.get(i);
-			Map<String, Object> propConf = configProperties.get(p);
-			if(propConf != null) //eg. "name" property.
-				score += compareProperties(p, props1.get(p), props2.get(p), propConf);
-		}
+
+	//	TODO: finish on xml comparison; do more testing
+	//	double xmlComparisonScore = compareSourceDocuments(vertexOne.getString("sourceDocument"), vertexTwo.getString("sourceDocument"), vertexOntology);
+		
 		return score;
 	}
 
-	public static double compareProperties (String propertyName, Object property1, Object property2, Map<String, Object> configProperties) {
-		if(property1 == null || property2 == null)
-			return 0;
-
-		//Comparison comparison;
-		WHIRL whirl;
-
-		Logger logger = LoggerFactory.getLogger(Align.class);
-		//comparison = new Comparison();
-
-		Double score = 0.0;
-		Double comparisonWeight = 1.0;
-		String comparisonFunction = new String("");
-		comparisonFunction = configProperties.get("comparisonFunction").toString();
-		comparisonWeight = new Double (configProperties.get("comparisonWeight").toString());
-
-		//was going to use a switch statement, but switch with strings is not supported by eclipse
-		if (comparisonFunction.equals("exact"))	{
-			if (property1.equals(property2))
-				score = score + 1 * comparisonWeight;
+	private double compareProperties(Object propertyOne, Object propertyTwo, JSONObject propertyOntology) {
+		double comparisonWeight = propertyOntology.optDouble("comparisonWeight", 1.0);
+		String comparisonFunction = propertyOntology.getString("comparisonFunction");
+		if (comparisonFunction.equals("exact")) {
+			return ((propertyOne.equals(propertyTwo)) ? 1.0 : 0.0) * comparisonWeight;
 		}
-		//time should be standardized  
+		if (comparisonFunction.equals("WHIRL")) {
+			//TODO After collecting big anough database, create config file for whirl and remove cosine similarity comparison; 
+   			//     up until then, use greatest between cosine similarity and whirl
+			whirl = (whirl == null) ? new WHIRL() : whirl;
+			double whirlScore =  comparisonWeight * whirl.compareObjects(propertyOne.toString(), propertyTwo.toString());
+			cs = (cs == null) ? new CosineSimilarity() : cs;
+			double csScore = cs.getSimilarityScore(propertyOne.toString(), propertyTwo.toString());
+			return (whirlScore > csScore) ? whirlScore : csScore;
+		}
 		if (comparisonFunction.equals("compareTimestamps"))	{
 			//TODO: timestamps need to be standardized before this can happen
-			Long timestamp1 = null;
-			Long timestamp2 = null;
-			try{
-				if(property1 instanceof Long)
-					timestamp1 = (Long)property1;
-				if(property2 instanceof Long)
-					timestamp2 = (Long)property2;
-				if(property1 instanceof Integer)
-					timestamp1 = Long.valueOf((Integer)property1);
-				if(property2 instanceof Integer)
-					timestamp2 = Long.valueOf((Integer)property2);
-				if(property1 instanceof String)
-					timestamp1 = Long.valueOf((String)property1);
-				if(property2 instanceof String)
-					timestamp2 = Long.valueOf((String)property2);
-			}catch(ClassCastException e){
-				logger.warn("Could not convert timestamps: bad format?  Timestamps are: " + property1 + " " + property2);
-				logger.warn("exception is: ", e);
-			}catch(NumberFormatException e){
-				logger.warn("Could not convert timestamps: bad format?  Timestamps are: " + property1 + " " + property2);
-				logger.warn("exception is: ", e);
-			}
-			if(timestamp1 != null && timestamp2 != null)
-				score = score + compareDate(timestamp1, timestamp2) * comparisonWeight;
+			// 	work on timestamp ... in stix it all is in xml GregorianCalendar format 
+			Long timestampOne = null;
+			Long timestampTwo = null;
 		}
 		//complaining that it is the array list, but function is working with jsonarray
 		if (comparisonFunction.equals("compareReferences"))	{
-			score = score + compareReferences(property1, property2) * comparisonWeight;	
+			return compareReferences(propertyOne, propertyTwo) * comparisonWeight;	
 		}
 		if (comparisonFunction.equals("SmithWaterman"))	{
 			SmithWaterman sw = new SmithWaterman();
-			double newScore = sw.smithWatermanScore(property1.toString(), property2.toString());
-			score = score + newScore * comparisonWeight;
+			return sw.smithWatermanScore(propertyOne.toString(), propertyTwo.toString()) * comparisonWeight;
 		}
 
-		if (comparisonFunction.equals("WHIRL"))	{
-			whirl = new WHIRL();
-			double w = whirl.compareObjects(property1.toString(), property2.toString());
-			score = score + w * comparisonWeight;
-		}
-
-		//need to write one for whirl
-		return score;
+		return 0.0;
 	}
-
+	
 	//return is between 0.0 (nothing in common) and 1.0
 	static double compareReferences (Object o1, Object o2)	{
 
-		if (o1 == null || o2 == null)	return 0.0;
-		int match = 0, total = 0;
+		if (o1 == null || o2 == null) {
+			return 0.0;
+		}
+		int match = 0;
+		int total = 0;
 
 		ArrayList a1 = (ArrayList) o1;
 		ArrayList a2 = (ArrayList) o2;
@@ -126,137 +93,73 @@ public class Compare {
 				}
 			}
 		}
-		if(match == 0 ) 
-			return 0;
-		else
-			return (double)(match)/(double)total;
+		return (match == 0 ) ? 0 : ((double)match)/((double)total);
 	}
 
 	public static double compareDate (long timeOne, long timeTwo)	{
-		if (timeOne == timeTwo) return 1.0;
-		else return 1.0/(double)Math.abs(timeOne - timeTwo);
+		return (timeOne == timeTwo) ? 1.0 : 1.0/(double)Math.abs(timeOne - timeTwo);
+	}
+	
+	/* function compares two xml source documents traversing them along with relevant vertexOntology 
+	   with comparison rules mapped to many of xml elements; if rule is not specified, algorithm is using 
+	   a default comparison */
+	private double compareSourceDocuments(String sourceDocOne, String sourceDocTwo, JSONObject vertexOntology) {
+		double score = 0.0;
+		Document docOne = parseXMLText(sourceDocOne);
+		Document docTwo = parseXMLText(sourceDocTwo);
+		Element rootElementOne = docOne.getRootElement();
+		Element rootElementTwo = docTwo.getRootElement();
+		JSONObject comparisonRules = vertexOntology.getJSONObject("comparisonRules").getJSONObject(rootElementOne.getName());
+		score = compareDocumentElements(rootElementOne, rootElementTwo, comparisonRules);		
+
+		return score;
 	}
 
-	/*
-    public boolean findDuplicateVertex(String vertices)	{
-		double maxScore = 0.0;
-		String idOfDuplicateOne;
-		String idOfDuplicateTwo;
-		JSONObject graphson = new JSONObject(vertices);
-		JSONArray jsonVerts = graphson.optJSONArray("vertices");
-
-		if (jsonVerts != null)	{
-
-			//creating two maps: existing graph, and new input
-			// with key = "_id" and value = "description" for the WHIRL function								
-			Map<String, String> descriptionExisting = getExistingVerticesDescriptionMap();
-			Map<String, String> descriptionNew = getNewVerticesDescriptionMap(jsonVerts);
-			whirl.setTextMaps(descriptionExisting, descriptionNew);
-
-			Map<String, String> vertIDCache = align.getIDCache();
-
-    		for (String idOne : vertIDCache.keySet())	{
-    			double score = 0.0;
-				Map<String, Object> existingVertex = align.getVertByID(vertIDCache.get(idOne).toString());
-
-			for (int i = 0; i < jsonVerts.length(); i++)	{
-				JSONObject newVertex = (JSONObject) jsonVerts.get(i);
-				Map<String, Map<String, Object>> property = align.getConfig(newVertex);
-    				for (Object s : newVertex.keySet()) {
-    					if (existingVertex.get(s) != null) {
-							if (property.get(s) != null)	{
-								Map<String, Object> configProperties = (Map<String, Object>) property.get(s);
-							// in bugtraq _id == CVE
-							String idTwo = newVertex.get("CVE").toString();
-    							score = Compare.comparisonScore (s.toString(), idOne, idTwo, existingVertex.get(s), newVertex.get(s.toString()), configProperties);
-
-							//	if (maxScore < score)	{	
-							//		score = maxScore;
-						//		idOfDuplicate = idTwo;
-   						//	}
-						//	if (score > 0.75)	System.out.println("idOne = " + idOne + " idTwo = " + idTwo + " score = " + score);
-							}
+	/* compares two elements according comparisonRules from ontology specified for vertexTypes;	
+	   function applies knows comparison rules to text fields of elements, if comparison rules do not specify, 
+	   then it applies default comparison function */
+	private double compareDocumentElements(Element elementOne, Element elementTwo, JSONObject comparisonRules) {
+		double score = 0.0;
+		printElement(elementOne);
+		List<Element> childrenOne = elementOne.getChildren();
+		// TODO: check if element contains an array of children elements with the same name ... it will make a difference
+		for (Element childOne : childrenOne) {
+			Element childTwo = null;
+			if ((childTwo = elementTwo.getChild(childOne.getName(), childOne.getNamespace())) != null) {
+				if (!childOne.getText().isEmpty() && !childTwo.getText().isEmpty()) {
+					if (comparisonRules ==  null) {
+						score = score + defaultComparison(childOne.getText(), childTwo.getText());
+					} else {
+						if (comparisonRules.has(childOne.getName())) {
+							JSONObject comparisonSpecifications = comparisonRules.getJSONObject(childOne.getName());
+							if (comparisonSpecifications.has("comparisonFunction")) {
+								score = score + compareProperties(childOne.getText(), childTwo.getText(), comparisonSpecifications);
+							} else {
+								score = score + defaultComparison(childOne.getText(), childTwo.getText());
+							}	
 						}
-    				}
+					}
+				} else {
+					score = score + compareDocumentElements(childOne, childTwo, comparisonRules.optJSONObject(childOne.getName()));
 				}
 			}
-		}
-    		//set threshold	    		
-    	//	if (maxScore > 0.75)	{	//=> duplicate;
-    	//		Map<String, String> mergeMehods = computeMerchMethodMap (property);
-    			//updating properties of duplicate based on config file
-    	//		align.alignVertProps(idOfDuplicate, (Map<String, Object>) vertex, mergeMethods);	
+		}		
 
-		//}
-
-
-    	return true;
-    }
-	 */
-
-	/*
-	//unneeded, everything in use is static...
-	public Compare(Align align)	{
-		this.align = align;
-		this.logger = LoggerFactory.getLogger(Align.class);
-		comparison = new Comparison();
-		whirl = new WHIRL();
+		return score;
 	}
-	 */
 
-	/*
-	//setting map of existing descriptions keyed on id
-	//unused
-	Map<String, String>  getExistingVerticesDescriptionMap()	{
+	/* default comparison for values without specified comparison functions;
+	   it is using exact comparison if both values are numeric, 
+	   and cosine similarity if both values are strings */
+	private double defaultComparison(String strOne, String strTwo) {
+		if (StringUtils.isNumeric(strOne) && StringUtils.isNumeric(strTwo)) {
+			return (strOne.equals(strTwo)) ? 1.0 : 0.0;
+		} 
+		if (!StringUtils.isNumeric(strOne) && !StringUtils.isNumeric(strTwo)) {
+			cs = (cs == null) ? new CosineSimilarity() : cs;
+			return cs.getSimilarityScore(strOne, strTwo);
+		} 
 
-		Map<String, String> description = new HashMap<String, String>();	
-		Map<String, String> vertIDCache = align.getIDCache();
-
-	    for (String id : vertIDCache.keySet())	{ //TODO: can't assume cache will have all needed verts
-			Map<String, Object> existingVertex = align.getVertByID(vertIDCache.get(id));
-			if (existingVertex.get("description") != null)	
-				description.put(id, existingVertex.get("description").toString());
-		}
-		return description;
+		return 0.0;
 	}
-	 */
-
-	/*
-	//making map of descriptions keyed on id for new vertexes
-	//unused
-	Map<String, String> getNewVerticesDescriptionMap(JSONArray newVertex)	{
-
-		Map<String, String> description = new HashMap<String, String>();
-		JSONObject vertex = new JSONObject();
-
-		for (int i = 0; i < newVertex.length(); i++)	{
-			vertex = (JSONObject) newVertex.get(i);
-			if (vertex.has("description") && vertex.has("_id"))	{
-				String id = vertex.getString("_id");		
-				description.put(id, vertex.getString("description"));
-			}
-		}
-
-		return description;
-	}
-	 */
-
-	/*
-	//unused
-	public Map<String, String> computeMergeMethodMap (Map<String, Map<String, Object>> property)	{
-
-		Map<String, Object> temp = new HashMap<String, Object>();
-		Map<String, String> mergeMethods = new HashMap<String, String>();
-		String renewProperty;
-
-		for (String key : property.keySet())	{
-			temp = (Map<String, Object>) property.get(key);
-			renewProperty = temp.get("resolutionFunction").toString();
-			mergeMethods.put(key, renewProperty);
-		}
-
-		return mergeMethods;
-	}
-	 */
-
 }
