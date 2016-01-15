24e620877287243f5ca838c8c0536ce660dcd327
Maria Vincent <mvincen2@vols.utk.edu>
2016-01-15 13:10:28 -0500

    loaded for stucco_ontology and graph_ontology to build and validate stucco graph

--- src/main/java/alignment/alignment_v2/ConfigFileLoader.java
+++ src/main/java/alignment/alignment_v2/ConfigFileLoader.java
@@ -1,5 +1,8 @@
 package alignment.alignment_v2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import java.io.IOException;
 
 import java.nio.file.Files;
@@ -9,36 +12,50 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 
 public class ConfigFileLoader {
-	private String STIX_ONTOLOGY = "resources/ontology/stix_ontology.json";
-	private String VERTEX_TYPE_CONFIG = "resources/ontology/vertex_type_config.json";
+	private String STUCCO_ONTOLOGY = "resources/ontology/stucco_ontology.json";
+	private String GRAPH_CONFIG = "resources/ontology/graph_config.json";
 	
-	private JSONObject ontology = null;
-	private JSONObject vertexTypeConfig = null;
+	private JSONObject stuccoOntology = null;
+	private JSONObject graphConfig = null;
 
 	public ConfigFileLoader() {
 		try {
-			/* required to map new incomming stix xml to vertesType (like is it IP, Port, etc ?) */
-			vertexTypeConfig = new JSONObject(new String(Files.readAllBytes(Paths.get(VERTEX_TYPE_CONFIG))));
-			/* required to construct a graph, to do all the comparisons, and stix xml editing */
-			ontology = new JSONObject(new String(Files.readAllBytes(Paths.get(STIX_ONTOLOGY))));
+			/* required to map new incomming stix xml to json vertex */
+			stuccoOntology = new JSONObject(new String(Files.readAllBytes(Paths.get(STUCCO_ONTOLOGY))));
+			/* required to determine vertexTypes and edges to construct a graph */
+			graphConfig = new JSONObject(new String(Files.readAllBytes(Paths.get(GRAPH_CONFIG))));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public JSONObject getStuccoOntology() {
-		return ontology;
+		return stuccoOntology;
 	}
 	
-	public JSONObject getVertexTypeConfig() {
-		return vertexTypeConfig;
+	public JSONObject getGraphConfig() {
+		return graphConfig;
 	}
-
+	
 	public JSONObject getVertexOntology(String vertexType) {
-		if (ontology.getJSONObject("definitions").has(vertexType)) {
-			return ontology.getJSONObject("definitions").getJSONObject(vertexType);
+		if (stuccoOntology.getJSONObject("definitions").has(vertexType)) {
+			return stuccoOntology.getJSONObject("definitions").getJSONObject(vertexType);
 		} 
 		
 		return null;
+	}
+
+	public List<String> getRelationsByVertexType(String vertexType) {
+		JSONObject vertexConfig = graphConfig.optJSONObject(vertexType);
+		if (vertexConfig == null) {
+			return null;
+		} 
+		List<String> relationList = new ArrayList<String>();
+		JSONObject edges = vertexConfig.optJSONObject("stixEdges");
+		for (Object key : edges.keySet()) {
+			relationList.add(key.toString());
+		}
+
+		return relationList;
 	}
 }
