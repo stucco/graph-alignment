811fad12c326737ebcae511958a1a6b5f10508fe
Maria Vincent <mvincen2@vols.utk.edu>
2016-04-14 20:29:23 -0400

    changed config loader to abstract class with static initialization

--- src/main/java/alignment/alignment_v2/ConfigFileLoader.java
+++ src/main/java/alignment/alignment_v2/ConfigFileLoader.java
@@ -1,9 +1,5 @@
 package alignment.alignment_v2;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
-
 import java.io.IOException;
 
 import java.nio.file.Files;
@@ -12,37 +8,26 @@ import java.nio.file.Paths;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-public class ConfigFileLoader {
-	private String STUCCO_ONTOLOGY = "resources/ontology/stucco_ontology.json";
-	private String GRAPH_CONFIG = "resources/ontology/graph_config.json";
-	private String OBSERVABLE_TYPES = "resources/ontology/observable_types.json"; 
-	
-	private JSONObject stuccoOntology = null;
-	private JSONObject graphConfig = null;
-	private JSONObject observableTypes = null;
+abstract class ConfigFileLoader {
 
-	public ConfigFileLoader() {
+	private static final String _stucco_ontology = "resources/ontology/stucco_ontology.json";
+	private static final String _cybox_objects = "resources/ontology/cybox_ontology.json";
+	
+	static JSONObject stuccoOntology;
+	static JSONObject cyboxObjects; 
+
+	static {
 		try {
 			/* required to map new incomming stix xml to json vertex */
-			stuccoOntology = new JSONObject(new String(Files.readAllBytes(Paths.get(STUCCO_ONTOLOGY))));
-			/* required to determine vertexTypes and edges to construct a graph */
-			graphConfig = new JSONObject(new String(Files.readAllBytes(Paths.get(GRAPH_CONFIG))));
+			stuccoOntology = new JSONObject(new String(Files.readAllBytes(Paths.get(_stucco_ontology))));
 			/* required to determine observable type (File, URL, etc.) and provides path to extract name for every type */
-			observableTypes = new JSONObject(new String(Files.readAllBytes(Paths.get(OBSERVABLE_TYPES))));
+			cyboxObjects = new JSONObject(new String(Files.readAllBytes(Paths.get(_cybox_objects))));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-	public JSONObject getStuccoOntology() {
-		return stuccoOntology;
-	}
 	
-	public JSONObject getGraphConfig() {
-		return graphConfig;
-	}
-	
-	public JSONObject getVertexOntology(String vertexType) {
+	public static JSONObject getVertexOntology(String vertexType) {
 		if (stuccoOntology.getJSONObject("definitions").has(vertexType)) {
 			return stuccoOntology.getJSONObject("definitions").getJSONObject(vertexType);
 		} 
@@ -50,21 +35,16 @@ public class ConfigFileLoader {
 		return null;
 	}
 
-	public List<String> getRelationsByVertexType(String vertexType) {
-		JSONObject vertexConfig = graphConfig.optJSONObject(vertexType);
-		if (vertexConfig == null) {
-			return null;
-		} 
-		List<String> relationList = new ArrayList<String>();
-		JSONObject edges = vertexConfig.optJSONObject("stixEdges");
-		for (Object key : edges.keySet()) {
-			relationList.add(key.toString());
-		}
-
-		return relationList;
+	public static JSONObject getObservableType(String type) { 
+		return cyboxObjects.optJSONObject(type);
 	}
-
-	public JSONObject getObservableType(String type) { 
-		return observableTypes.optJSONObject(type);
+	public static JSONObject loadConfig(String fileName) {
+		try {
+			return new JSONObject(new String(Files.readAllBytes(Paths.get(fileName))));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
