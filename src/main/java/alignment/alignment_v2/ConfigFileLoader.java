8147c43986d200447646ae3f6eb572c573f194c9
Maria Vincent <mvincen2@vols.utk.edu>
2016-04-27 16:46:07 -0400

    updated resources in pom file

--- src/main/java/alignment/alignment_v2/ConfigFileLoader.java
+++ src/main/java/alignment/alignment_v2/ConfigFileLoader.java
@@ -2,18 +2,14 @@ package alignment.alignment_v2;
 
 import java.io.IOException;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import org.apache.commons.io.IOUtils;
 
 import org.json.JSONObject;
-import org.json.JSONArray;
 
 public abstract class ConfigFileLoader {
 
-	private static final String _stucco_ontology = "/resources/ontology/stucco_ontology.json";
-	private static final String _cybox_objects = "/resources/ontology/cybox_ontology.json";
+	private static final String _stucco_ontology = "ontology/stucco_ontology.json";
+	private static final String _cybox_objects = "ontology/cybox_ontology.json";
 
 	public static JSONObject stuccoOntology;
 	public static JSONObject cyboxObjects; 
@@ -21,12 +17,12 @@ public abstract class ConfigFileLoader {
 	static {
 		try {
 				/* required to map new incomming stix xml to json vertex */
-				stuccoOntology = new JSONObject(IOUtils.toString(ConfigFileLoader.class.getResourceAsStream(_stucco_ontology), "UTF-8"));
+				stuccoOntology = new JSONObject(IOUtils.toString(ConfigFileLoader.class.getClassLoader().getResourceAsStream(_stucco_ontology), "UTF-8"));
 				/* required to determine observable type (File, URL, etc.) and provides path to extract name for every type */
-				cyboxObjects = new JSONObject(IOUtils.toString(ConfigFileLoader.class.getResourceAsStream(_cybox_objects), "UTF-8"));
+				cyboxObjects = new JSONObject(IOUtils.toString(ConfigFileLoader.class.getClassLoader().getResourceAsStream(_cybox_objects), "UTF-8"));
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		} 
 	}
 	
 	public static JSONObject getVertexOntology(String vertexType) {
@@ -39,14 +35,5 @@ public abstract class ConfigFileLoader {
 
 	public static JSONObject getObservableType(String type) { 
 		return cyboxObjects.optJSONObject(type);
-	}
-	public static JSONObject loadConfig(String fileName) {
-		try {
-			return new JSONObject(new String(Files.readAllBytes(Paths.get(fileName))));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
 	}
 }
